eaaffb9e317235b563e0fd2cb3a617d2ef069981
Demitri Muna <github@demitri.com>
2021-07-28 15:00:26 -0400

    Fixing method parameter default errors.

--- source/scidd/core/api.py
+++ source/scidd/core/api.py
@@ -1,6 +1,7 @@
 
 import os
-from typing import Dict
+import pathlib
+from typing import Dict, Union
 
 import requests
 
@@ -8,17 +9,17 @@ from scidd.core.utilities.designpatterns import singleton
 
 @singleton
 class API:
-	
+
 	def __init__(self, host:str="api.trillianverse.org", port:int=443):
-		
+
 		self.host = host
 		self.port = port
-		
+
 		if host is None and "SCIDD_API_HOST" in os.environ:
 			self.host = os.environ["SCIDD_API_HOST"]
 		if port is None and "SCIDD_API_PORT" in os.environ:
 			self.port = os.environ["SCIDD_API_PORT"]
-		
+
 		if self.host in ["127.0.0.1", "localhost"]:
 			self.scheme = "http://" # for development
 		else:
@@ -31,30 +32,32 @@ class API:
 		'''
 		return f"{self.scheme}{self.host}:{self.port}"
 
-	def get(self, path=None, params=[], headers:dict=None) -> dict:
+	def get(self, path:Union[str, pathlib.Path], params:dict=None, headers:dict=None) -> dict:
 		'''
 		Make a GET call on the SciDD API with the given path and parameters.
-		
+
 		:param path: the path of the API to call
 		:param params: a dictionary of the parameters to pass to the API
 		:param headers: any additional headers to pass to the API
 		:returns: JSON response
 		:raises: see: https://2.python-requests.org/en/master/api/#exceptions
 		'''
-		if path is None:
-			raise ValueError("A path must be provided to make an API call.")
-		
+		#if path is None:
+		#	raise ValueError("A path must be provided to make an API call.")
+		if params is None:
+			params = list()
+
 		with requests.Session() as http_session:
 		#	try:
 			response = http_session.get(self.base_url + path, params=params)
 			response.raise_for_status()
-		
+
 		return response.json()
 
-	def post(self, path:str=None, params:dict={}, data:dict={}, headers:Dict[str,str]=None) -> dict:
+	def post(self, path:Union[str, pathlib.Path], params:dict=None, data:dict=None, headers:Dict[str,str]=None) -> dict:
 		'''
 		Make a POST call on the Trillian API with the given path, parameters, and body.
-		
+
 		:param path: the path of the API to call
 		:param params: a dictionary of the parameters to pass to the API
 		:param data: data to be passed as the body of the call
@@ -62,8 +65,15 @@ class API:
 		:returns: JSON response
 		:raises: see: https://2.python-requests.org/en/master/api/#exceptions
 		'''
-		if path is None:
-			raise ValueError("A path must be provided to make an API call.")
+		#if path is None:
+		#	raise ValueError("A path must be provided to make an API call.")
+
+		if params is None:
+			params = dict()
+		if data is None:
+			data = dict()
+		if headers is None:
+			header = dict()
 
 		with requests.Session() as http_session:
 			response = http_session.post(self.base_url + path, params=params, data=data)
